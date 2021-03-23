7d01c77bde3fecf727e08f075dd673fa1d44c7e5
gschadegg <gschadegg@gmail.com>
2021-03-23 15:49:34 -0400

    removed unneed definitions

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet'
 import LocationMarker from './LocationMarker'
 
 import styles from './Map.module.scss'
